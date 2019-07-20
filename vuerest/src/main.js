60d0c51a16b1b5c042be482fa5b0d915445046bc
Mastermind-U <rex49513@gmail.com>
2019-07-20 23:17:27 +0300

    new vue version

--- vuerest/src/main.js
+++ vuerest/src/main.js
@@ -1,20 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+
 import MuseUI from 'muse-ui';
 import 'muse-ui/dist/muse-ui.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'material-icons/iconfont/material-icons.css';
+
+window.$ = window.jQuery = require('jquery');
+
 Vue.use(MuseUI);
 Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
